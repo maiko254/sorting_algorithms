0c83e1b2228113c7cf05a3f54218fbb2d5a45cfb
Michael Bonyo <bonyomichael406@gmail.com>
2023-01-23 11:13:38 -0800

    correction Task 0

--- sort.h
+++ sort.h
@@ -5,7 +5,7 @@
 
 /**
  * struct listint_s - Doubly linked list node
- * 
+ *
  * @n: Integer stored in the node
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
