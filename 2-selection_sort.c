7c010b5ef01015a303d66674af6f10dcf13b56a2
maiko254 <bonyomichael406.com>
2024-01-22 11:00:10 +0300

    sorting an array of integers using selection sort algorithm

---
+++ 2-selection_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array of integers using selectio sort algorithm
+ * @array: array to sort
+ * @size: size of the array
+ *
+ * Return: void
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min;
+	int temp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+			if (array[j] < array[min])
+				min = j;
+		if (min != i)
+		{
+			temp = array[min];
+			array[min] = array[i];
+			array[i] = temp;
+			print_array(array, size);
+		}
+	}
+}
